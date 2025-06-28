a586ea7fde45cbf231550b7e1390b7d59db88d4e
Groupe12-Girls <zerrouki.cylia@gmail.com>
2025-06-28 16:35:27 +0200

    feat: consumers RabbitMQ pour les produits (created, updated, deleted)

--- app/messaging/rabbitmq.py
+++ app/messaging/rabbitmq.py
@@ -74,4 +74,54 @@ def publish_order_deleted(order_id: str, channel=None):
     )
 
     if close_connection:
-        connection.close()            
+        connection.close()  
+
+# Consumers
+
+# Consumer du publish_product_created
+
+def consume_product_created(callback):
+    connection = pika.BlockingConnection(pika.URLParameters(settings.RABBITMQ_URL))
+    channel = connection.channel()
+    channel.queue_declare(queue="product_created", durable=True)
+
+    def wrapper(ch, method, properties, body):
+        data = json.loads(body)
+        callback(data)
+        ch.basic_ack(delivery_tag=method.delivery_tag)
+
+    channel.basic_consume(queue="product_created", on_message_callback=wrapper)
+    print("[*] En attente de messages sur 'product_created'. CTRL+C pour arrêter.")
+    channel.start_consuming()
+
+# Consumer du publish_product_updated
+
+def consume_product_updated(callback):
+    connection = pika.BlockingConnection(pika.URLParameters(settings.RABBITMQ_URL))
+    channel = connection.channel()
+    channel.queue_declare(queue="product_updated", durable=True)
+
+    def wrapper(ch, method, properties, body):
+        data = json.loads(body)
+        callback(data)
+        ch.basic_ack(delivery_tag=method.delivery_tag)
+
+    channel.basic_consume(queue="product_updated", on_message_callback=wrapper)
+    print("[*] En attente de messages sur 'product_updated'. CTRL+C pour arrêter.")
+    channel.start_consuming()
+
+# Consumer de publish_product_deleted
+
+def consume_product_deleted(callback):
+    connection = pika.BlockingConnection(pika.URLParameters(settings.RABBITMQ_URL))
+    channel = connection.channel()
+    channel.queue_declare(queue="product_deleted", durable=True)
+
+    def wrapper(ch, method, properties, body):
+        data = json.loads(body)
+        callback(data)
+        ch.basic_ack(delivery_tag=method.delivery_tag)
+
+    channel.basic_consume(queue="product_deleted", on_message_callback=wrapper)
+    print("[*] En attente de messages sur 'product_deleted'. CTRL+C pour arrêter.")
+    channel.start_consuming()    
