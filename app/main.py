426200603cc9adb04c8000f05ab8e869b1be4453
Groupe12-Girls <zerrouki.cylia@gmail.com>
2025-07-03 18:03:02 +0200

    Restauration de main.py depuis feature/routes avant fusion

---
+++ app/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from app.routes import orders, token
+from prometheus_fastapi_instrumentator import Instrumentator
+
+# Création de l'application FastAPI avec métadonnées
+app = FastAPI(
+    title="Orders API",                      # Nom affiché dans Swagger
+    version="1.0.0",                         # Version de l'API
+    description="API de gestion des commandes avec monitoring Prometheus"
+)
+
+# Instrumentation Prometheus pour exposer les métriques
+# Accessible sur /metrics
+Instrumentator().instrument(app).expose(app)
+
+# Ajout du routeur des commandes
+app.include_router(orders.router)
+
+# Route /token pour l’authentification JWT
+app.include_router(token.router)
